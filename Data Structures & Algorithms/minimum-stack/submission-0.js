8c24aa2c13812b2fc82fed5bfc69ce46f4650037
Chandrasekar Gokulanathan <chandrasekarg@outlook.com>
2026-08-12 15:03:34 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,41 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        let minVal = val;
+        if(this.minStack.length !== 0) {
+            minVal = Math.min(minVal, this.getMin());
+        }
+        this.minStack.push(minVal);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1];
+    }
+}
