10df90deb51c4968e8b9833f484ecfd114f7b33a
Chandrasekar Gokulanathan <chandrasekarg@outlook.com>
2026-09-26 16:01:36 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n) {
+        const set = new Set();
+        let ans = this.findSumOfSquares(n);
+        let answerFound = false;
+        while(!answerFound) {
+            console.log("hi there");
+            if(ans == 1) {
+                answerFound = true;
+                return true;
+            }
+            else if (set.has(ans)) {
+                console.log(set);
+                answerFound = true;
+                return false;
+            }
+            else {
+                set.add(ans);
+                ans = this.findSumOfSquares(ans);
+            }
+        }
+        
+    }
+
+    findSumOfSquares(n) {
+        const digits = n.toString().split("");
+        let result = 0;        
+        digits.forEach(digit => {
+            var no = Number(digit);
+            result += (no * no);
+        });
+        console.log("Input - " + n + " Digits " + digits + " Output - " + result  );
+        return result;
+    }
+}
