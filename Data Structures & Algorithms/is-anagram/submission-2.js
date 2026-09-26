0a382f227edee7466c94081c1bd510c1903f6f74
Chandrasekar Gokulanathan <chandrasekarg@outlook.com>
2026-09-26 16:34:06 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length != t.length) return false;
+
+        const map = new Map();
+
+        for(let char of s) {
+            if(map.has(char)) {
+                map.set(char, map.get(char) + 1);
+            } else {
+                map.set(char, 1);
+            }
+        }
+
+        for(let char of t) {
+            if(!map.has(char) || map.get(char) < 1) return false;
+            map.set(char, map.get(char) - 1);
+        }
+
+        return true;
+    }
+}
